9d12c77c28f74cf9f6eb796fa596d34d0575f293
Artsiom <artem.dubinka@kreo.net>
2024-09-02 10:32:49 +0300

    add

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Hello World!"
+# Получение значений из переменных окружения GitHub Actions
+DOCKER_IMAGE="4836297/postgres-application:$GITHUB_RUN_NUMBER"
+
+# Вывод значений на экран
+echo "Docker Image Tag: $DOCKER_IMAGE"
